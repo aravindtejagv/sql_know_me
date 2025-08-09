323b2f4d4278660c4a889fbf518cde26e1242a95
aravindtejagv <84582728+aravindtejagv@users.noreply.github.com>
2025-08-09 17:57:02 +0530

    new rlf

---
+++ Scripts/07_ROWLEVELFUNCTIONs.sql
@@ -0,0 +1,36 @@
+/* We have Functions in SQL in order to Manipulate, Analyze, Clean or Transform data inside table.
+
+TWO Categories of functions we have:
+
+1.  Single row functions eg: LOWER()
+2.  Multi row functrions eg: SUM()
+*/
+
+/* ============================================================================== 
+   SQL String Functions
+-------------------------------------------------------------------------------
+   This document provides an overview of SQL string functions, which allow 
+   manipulation, transformation, and extraction of text data efficiently.
+
+   Table of Contents:
+     1. Manipulations
+        - CONCAT
+        - LOWER
+        - UPPER
+	- TRIM
+	- REPLACE
+     2. Calculation
+        - LEN
+     3. Substring Extraction
+        - LEFT
+        - RIGHT
+        - SUBSTRING
+=================================================================================
+*/
+
+/* ============================================================================== 
+   CONCAT() - String Concatenation
+=============================================================================== */
+
+-- Concatenate first name and country into one column
+
